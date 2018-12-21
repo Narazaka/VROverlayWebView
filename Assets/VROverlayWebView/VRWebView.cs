30c4558f89b0e41120f61de0d5cca434c0961021
Narazaka <info@narazaka.net>
2018-12-21 16:20:24 +0900

    VROverlayWebView

---
+++ Assets/VROverlayWebView/VRWebView.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using ICODES.STUDIO.WWebView;
+using sh_akira.VROverlay;
+
+namespace VROverlayWebView {
+    [RequireComponent(typeof(WWebView))]
+    public class VRWebView : MonoBehaviour {
+        bool IsDrag = false;
+        bool IsKeyUp = false;
+
+        IEnumerator Start() {
+            while (!AreYouReady) yield return null;
+
+            // webView.Alpha = 0.005f;
+            webView.Show();
+            webView.SetTexture(texture);
+
+            var renderer = GetComponent<MeshRenderer>();
+            if (renderer != null) renderer.material.mainTexture = texture;
+
+            var overray = GetComponent<VROverlay>();
+            if (overray != null) overray.texture = texture;
+        }
+
+        void Update() {
+
+        }
+
+        WWebView webView {
+            get {
+                return _webView ?? (_webView = GetComponent<WWebView>());
+            }
+        }
+
+        WWebView _webView;
+
+        Texture2D texture {
+            get {
+                if (_texture != null) return _texture;
+                _texture = new Texture2D(webView.GetActualWidth(), webView.GetActualHeight(), TextureFormat.ARGB32, false);
+                _texture.filterMode = FilterMode.Trilinear;
+                _texture.Apply();
+                return _texture;
+            }
+        }
+
+        Texture2D _texture;
+
+        bool AreYouReady {
+            get {
+                return (webView.GetActualWidth() > 0);
+            }
+        }
+    }
+}
---
+++ Assets/VROverlayWebView/VRWebViewInputModule.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using sh_akira.VROverlay;
+using ICODES.STUDIO.WWebView;
+
+namespace VROverlayWebView {
+    public class VRWebViewInputModule : BaseInputModule {
+        public OVRControllerAction controllerAction;
+        public List<VRGUIOverlay> guiOverlays = new List<VRGUIOverlay>();
+        public float scrollDeltaParam;
+
+        bool initialized = false;
+        bool IsDrag = false;
+        bool IsKeyDown = false;
+        bool IsKeyUp = false;
+        Vector2 previousPosition = Vector2.zero;
+        bool hasPreviousPosition = false;
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        public override void Process()
+        {
+            if (controllerAction == null) return;
+
+            if (initialized == false)
+            {
+                initialized = true;
+                controllerAction.KeyDownEvent += Controller_KeyDown;
+                controllerAction.KeyUpEvent += Controller_KeyUp;
+            }
+
+            foreach (var guiOverlay in guiOverlays) {
+                if (!guiOverlay.showCursor) continue;
+
+                var webView = guiOverlay.gameObject.GetComponent<WWebView>();
+                if (webView != null) {
+                    var position = guiOverlay.cursorPosition;
+                        var x1 = (int)(webView.GetActualWidth() * position.x);
+                        var y1 = (int)(webView.GetActualHeight() * position.y);
+                    if (IsKeyDown)
+                    {
+                        var x = (int)(webView.GetActualWidth() * position.x);
+                        var y = (int)(webView.GetActualHeight() * position.y);
+                        webView.InputEvent(1, 0, x, y);
+                    }
+                    if (IsKeyUp)
+                    {
+                        var x = (int)(webView.GetActualWidth() * position.x);
+                        var y = (int)(webView.GetActualHeight() * position.y);
+                        webView.InputEvent(2, 0, x, y);
+                    }
+                    if (IsDrag && hasPreviousPosition)
+                    {
+                        var delta = position.y - previousPosition.y;
+                        var x = (int)(webView.GetActualWidth() * position.x);
+                        var y = (int)(webView.GetActualHeight() * position.y);
+                        webView.InputEvent(3, (int)(webView.GetActualHeight() * delta * scrollDeltaParam), x, y);
+                    }
+                    previousPosition = position;
+                    hasPreviousPosition = true;
+                }
+            }
+
+            IsKeyDown = false;
+            IsKeyUp = false;
+        }
+
+        void Controller_KeyDown(object sender, OVRKeyEventArgs args)
+        {
+            if (args.ButtonId == Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger)
+            {
+                IsDrag = true;
+                IsKeyDown = true;
+            }
+        }
+
+        void Controller_KeyUp(object sender, OVRKeyEventArgs args)
+        {
+            if (args.ButtonId == Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger)
+            {
+                IsDrag = false;
+                IsKeyUp = true;
+            }
+        }
+    }
+}
